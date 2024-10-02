e90a1f8b8a1c7ef80673a20247b9fc979aa5da1c
Mahidhar Mogili <mahidharmogili@Mahidhars-MBP.bbrouter>
2024-10-02 21:00:50 +0530

    handled responsive ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,47 +11,99 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the screen height and width
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    final double verticalPadding = screenHeight * 0.05; // 5% vertical padding
+
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: const CommonAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 28.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.31, 1], // 31% stop for the white color
+            colors: [
+              Color(0xFFFFFFFF), // White at the top
+              Color(0xFFB1CBFF), // Light blue at the bottom
+            ],
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            const SizedBox(height: 175),
-            Image.asset('assets/images/girman_logo.png', height: 65),
-            const SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 10,
-                    offset: Offset(0, 4),
+            // Background Image positioned according to requirements
+            Positioned(
+              top: -28,
+              left: -252,
+              child: Opacity(
+                opacity: 0.75, // Background image with reduced opacity
+                child: Image.asset(
+                  'assets/images/home_bg_img.png', // Your background image asset
+                  width: 897,
+                  height: 897,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal:
+                      screenWidth * 0.07), // Responsive horizontal padding
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                      height:
+                          verticalPadding * 3.5), // Dynamic spacing at the top
+
+                  // Logo image
+                  Image.asset('assets/images/girman_logo.png', height: 65),
+
+                  SizedBox(height: verticalPadding), // Vertical padding
+
+                  // Search input
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                    ),
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Search',
+                        hintStyle: TextStyle(color: Colors.grey),
+                        prefixIcon: Icon(Icons.search, color: Colors.grey),
+                      ),
+                      onSubmitted: (query) {
+                        Provider.of<UserProvider>(context, listen: false)
+                            .setSearchQuery(query);
+
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                SearchResultScreen(initialQuery: query),
+                          ),
+                        );
+
+                        _searchController.clear();
+                      },
+                    ),
                   ),
                 ],
-              ),
-              child: TextField(
-                controller: _searchController,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search, color: Colors.grey),
-                ),
-                onSubmitted: (query) {
-                  Provider.of<UserProvider>(context, listen: false)
-                      .setSearchQuery(query);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SearchResultScreen(),
-                    ),
-                  );
-                },
               ),
             ),
           ],
--- lib/screens/search_results_screen.dart
+++ lib/screens/search_results_screen.dart
@@ -4,79 +4,138 @@ import 'package:girman_search_app/widgets/common_appbar.dart';
 import 'package:girman_search_app/widgets/user_card.dart';
 import 'package:provider/provider.dart';
 
-class SearchResultScreen extends StatelessWidget {
-  const SearchResultScreen({super.key});
+class SearchResultScreen extends StatefulWidget {
+  final String initialQuery; // Accept search query from HomeScreen
+
+  const SearchResultScreen({super.key, required this.initialQuery});
+
+  @override
+  _SearchResultScreenState createState() => _SearchResultScreenState();
+}
+
+class _SearchResultScreenState extends State<SearchResultScreen> {
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the search controller with the initial query from HomeScreen
+    _searchController = TextEditingController(text: widget.initialQuery);
+    // Set the search query in UserProvider to filter users based on the initial query
+    Provider.of<UserProvider>(context, listen: false)
+        .setSearchQuery(widget.initialQuery);
+  }
 
   @override
   Widget build(BuildContext context) {
     final users = Provider.of<UserProvider>(context).filteredUsers;
+    // Get the screen height and width for responsiveness
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final double verticalPadding = screenHeight * 0.05;
 
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: const CommonAppBar(),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 34.0),
-              child: Column(
-                children: [
-                  // Search input at the top
-                  Container(
-                    margin: const EdgeInsets.only(top: 100, bottom: 20),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12),
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 10,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Search',
-                        prefixIcon: Icon(Icons.search, color: Colors.grey),
-                      ),
-                      onChanged: (query) {
-                        Provider.of<UserProvider>(context, listen: false)
-                            .setSearchQuery(query);
-                      },
-                    ),
-                  ),
-                ],
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.31, 1],
+            colors: [
+              Color(0xFFFFFFFF),
+              Color(0xFFB1CBFF),
+            ],
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Positioned(
+              top: -28,
+              left: -252,
+              child: Opacity(
+                opacity: 0.75,
+                child: Image.asset(
+                  'assets/images/home_bg_img.png',
+                  width: 897,
+                  height: 897,
+                ),
               ),
             ),
-          ),
-          users.isNotEmpty
-              ? SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (context, index) {
-                      var user = users[index];
-                      return UserCard(user: user);
-                    },
-                    childCount: users.length,
-                  ),
-                )
-              : SliverToBoxAdapter(
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 100),
-                      child: Column(
-                        children: [
-                          Image.asset(
-                            'assets/images/empty_search.png',
-                            height: 300,
+            CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
+                    child: Column(
+                      children: [
+                        SizedBox(height: verticalPadding * 3),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black12,
+                                blurRadius: 10,
+                                offset: Offset(0, 4),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                          child: TextField(
+                            controller:
+                                _searchController, // Use the controller with the initial query
+                            decoration: const InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Search',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              prefixIcon:
+                                  Icon(Icons.search, color: Colors.grey),
+                            ),
+                            onChanged: (query) {
+                              Provider.of<UserProvider>(context, listen: false)
+                                  .setSearchQuery(query);
+                            },
+                          ),
+                        ),
+                        SizedBox(height: verticalPadding * 0.7),
+                      ],
                     ),
                   ),
                 ),
-        ],
+                users.isNotEmpty
+                    ? SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            var user = users[index];
+                            return UserCard(user: user);
+                          },
+                          childCount: users.length,
+                        ),
+                      )
+                    : SliverToBoxAdapter(
+                        child: Center(
+                          child: Padding(
+                            padding: EdgeInsets.only(top: verticalPadding * 2),
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/empty_search.png',
+                                  height: screenHeight * 0.3,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
